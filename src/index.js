85a4680447566a083bf3dd48628d2d0c149c2498
Sandosh Vasudevan <sandoshv@zillowgroup.com>
2020-02-25 11:09:46 -0800

    add loadder.io token

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const port = 80;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/health', (req, res) => res.send('I\'m Okay!'));
+app.get('/loaderio-154ed995e84cc7a2dcd063c1b64375f6.txt', (req, res) => res.send('loaderio-154ed995e84cc7a2dcd063c1b64375f6'));
 app.get('/test', async (req, res) => {
     dns.lookup('google.com', (err, address, family) => {
         if (err) {
